b5347b04c51b182b3e7d9aeefcc952a21b7be642
johnpyp <johnpyp.dev@gmail.com>
2024-05-24 19:33:32 -0700

    add kbd maja

---
+++ keyboards/kbdfans/maja/keymaps/johnpyp/rules.mk
@@ -0,0 +1,5 @@
+EXTRAKEY_ENABLE=yes          # Audio control and System control
+NO_USB_STARTUP_CHECK=yes # Maybe? https://github.com/qmk/qmk_firmware/issues/5585
+# BOOTMAGIC_ENABLE = no
+# VIA_ENABLE = yes
+# LTO_ENABLE = yes
